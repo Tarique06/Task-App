41a5384853d97c8dd991ccdef67a76105494a46f
Md Tarique <tarique@codelogicx.com>
2021-09-16 21:25:55 +0530

    Heroku changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,16 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "users",
-    dialect: "mysql",
+const url = process.env.DATABASE_URL
+const options = {
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
-};
+}
+
+if (!url) throw new Error('Missing environment variable: DATABASE_URL')
+
+module.exports = {
+    options,
+    url
+}
